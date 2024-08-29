a63a5db870e5a39e223115c06e8d808313f678ab
garikek <yan.buinov@gmail.com>
2024-08-29 17:01:34 +0300

    add swagger documentation

--- authentication-service/src/main/java/com/example/AuthenticationService/controller/AuthenticationController.java
+++ authentication-service/src/main/java/com/example/AuthenticationService/controller/AuthenticationController.java
@@ -12,15 +12,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "Authentication", description = "Endpoints for user authentication")
 public class AuthenticationController {
     private DefaultAuthenticationService authenticationService;
+
+    @Operation(summary = "Login", description = "Authenticate a user and return a JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Authentication successful"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PostMapping("/login")
     public ResponseEntity<JWTAuthResponse> login(@RequestBody JWTAuthRequest authRequest) throws UserNotFoundException, WrongPasswordException {
         return ResponseEntity.ok(authenticationService.login(authRequest));
     }
+
+    @Operation(summary = "Register", description = "Register a new user")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void register(@RequestBody UserDTO userDTO) throws AuthException {
